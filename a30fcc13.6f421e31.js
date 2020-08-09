0ff493ae5167c7087784853a4af12d1ccf5be6d1
SanketDG <sanketdasgupta@gmail.com>
2020-08-09 17:28:26 +0000

    Deploying to gh-pages from  @ f6079d9364e8398994d95df5a03ef2cab4ce61f5 🚀

---
+++ a30fcc13.6f421e31.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{60:function(n){n.exports=JSON.parse('{"docsSidebars":{"docs":[{"type":"link","label":"Introduction","href":"/tinysaurus/"}]},"permalinkToSidebar":{"/tinysaurus/":"docs"},"version":null}')}}]);
